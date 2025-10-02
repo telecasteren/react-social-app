19b42448b1538ac0e16a72b0f7bee1cd60b566e5
telecasteren <127958662+telecasteren@users.noreply.github.com>
2025-10-02 23:33:53 +0200

    fixed vitest with jsdom

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
-const jsdom = require("jsdom");
 
 export default defineConfig({
   test: {
     globals: true,
-    environment: jsdom,
+    environment: "jsdom",
     exclude: ["**/node_modules/**", "**/tests/e2e/**"],
   },
 });
